e0af0a1d7c830341619be5a545a6adc28188636e
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-05-01 15:07:03 +0100

    complete grid layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">Navbar</div>
+      <div className="sidebar">Sidebar</div>
+      <div className="content">
+        <div className="navbar">Filters</div>
+        <div className="items">
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+          <div className="item">Item</div>
+        </div>
+      </div>
     </div>
   );
 }
